ef8e9774ac1afba2888a9a1d399648d57944f74b
joshi <HIMANSHU@LAPTOP>
2024-01-12 18:15:04 +0530

    new react app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Todo from './Component/Todo'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Todo/>
+    </>
+  )
+}
+
+export default App
